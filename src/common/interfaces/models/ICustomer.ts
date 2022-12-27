9df5e1c1dd788a503b814c9ab21d34bf942f47c7
Kevin Darke <99612676+KangarooKafka@users.noreply.github.com>
2022-12-27 10:43:12 -0500

    Fleshed out model interfaces

--- src/common/interfaces/models/ICustomer.ts
+++ src/common/interfaces/models/ICustomer.ts
@@ -2,5 +2,16 @@ import { Document, Types} from 'mongoose';
 
 // Interface for the Customer model
 export default interface ICustomer extends Document {
-
+    firstName: string;
+    lastName: string;
+    address?: {
+        street: string;
+        city: string;
+        state: string;
+        zip: number;
+    };
+    phoneNumber?: string;
+    emailAddress?: string;
+    balanceDue: number;
+    booksCheckedOut: [Types.ObjectId]
 }
