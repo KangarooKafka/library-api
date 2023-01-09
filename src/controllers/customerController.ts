6d45b9bbda4dccf8ff289b6bb0883793309560c1
Kevin Darke <99612676+KangarooKafka@users.noreply.github.com>
2023-01-09 09:21:24 -0500

    Fleshed out the first iteration of the customer controller

--- src/controllers/customerController.ts
+++ src/controllers/customerController.ts
@@ -17,7 +17,30 @@ class CustomerController {
      * @param {() => Promise<void>} next The next client request.
      */
     public async addCustomer(ctx: RouterContext, next: () => Promise<void>): Promise<void> {
+        try {
+            // Create new customer entry
+            const customer: Document = await new Customer(ctx.request.body).save();
 
+            // Response to client
+            ctx.body = customer;
+            ctx.status = 201;
+
+            // Log results
+            logger.info(`Body: ${ctx.body}\nStatus: ${ctx.status}`);
+
+            // Clear req/res queue
+            await next();
+        } catch (e: any) {
+            // Response to client
+            ctx.body = e.message;
+            ctx.status = 500;
+
+            // Log results
+            logger.info(`Body: ${ctx.body}\nStatus: ${ctx.status}`);
+
+            // Clear req/res queue
+            await next();
+        }
     }
 
     /**
@@ -26,7 +49,43 @@ class CustomerController {
      * @param {() => Promise<void>} next The next client request.
      */
     public async getCustomer(ctx: RouterContext, next: () => Promise<void>): Promise<void> {
+        try {
+            // Get customer object
+            const customer = await Customer.findById(new Types.ObjectId(ctx.params.id));
 
+            // If customer is found
+            if(!_.isNil(customer)){
+                // Response to client
+                ctx.body = customer;
+                ctx.status = 200;
+
+                // Log results
+                logger.info(`Body: ${ctx.body}\nStatus: ${ctx.status}`);
+            }
+
+            // If customer is not found
+            else {
+                // Response to client
+                ctx.body = {message : "Customer not found"};
+                ctx.status = 404;
+
+                // Log results
+                logger.info(`Body: ${ctx.body}\nStatus: ${ctx.status}`);
+            }
+
+            // Clear req/res queue
+            await next();
+        } catch(e: any) {
+            // Response to client
+            ctx.body = e.message;
+            ctx.status = 500;
+
+            // Log results
+            logger.info(`Body: ${ctx.body}\nStatus: ${ctx.status}`);
+
+            // Clear req/res queue
+            await next();
+        }
     }
 
     /**
@@ -35,7 +94,30 @@ class CustomerController {
      * @param {() => Promise<void>} next The next client request.
      */
     public async getAllCustomers(ctx: RouterContext, next: () => Promise<void>): Promise<void> {
+        try {
+            // Get customers
+            const customers = await Customer.find({});
 
+            // Response to client
+            ctx.body = customers;
+            ctx.status = 200;
+
+            // Log results
+            logger.info(`Body: ${ctx.body}\nStatus: ${ctx.status}`)
+
+            // Clear req/res queue
+            await next();
+        } catch(e: any) {
+            // Response to client
+            ctx.body = e.message;
+            ctx.status = 500;
+
+            // Log results
+            logger.info(`Body: ${ctx.body}\nStatus: ${ctx.status}`);
+
+            // Clear req/res queue
+            await next();
+        }
     }
 
     /**
@@ -43,8 +125,44 @@ class CustomerController {
      * @param {RouterContext} ctx The request context object containing the new customer info.
      * @param {() => Promise<void>} next The next client request.
      */
-    public async updateCustomers(ctx: RouterContext, next: () => Promise<void>): Promise<void> {
+    public async updateCustomer(ctx: RouterContext, next: () => Promise<void>): Promise<void> {
+        try {
+            // Update customer
+            const customer: Document | null = await Customer.findByIdAndUpdate(new Types.ObjectId(ctx.params.id), ctx.request.body);
 
+            // If customer was found
+            if(!_.isNil(customer)) {
+                // Response to client
+                ctx.body = {message: "Success"};
+                ctx.status = 202;
+
+                // Log results
+                logger.info(`Body: ${ctx.body}\nStatus: ${ctx.status}`);
+            }
+
+            // If customer not found
+            else {
+                // Response to client
+                ctx.body = {message: "Customer not found"};
+                ctx.status = 404;
+
+                // Log results
+                logger.info(`Body: ${ctx.body}\nStatus: ${ctx.status}`);
+            }
+
+            // Clear req/res queue
+            await next();
+        } catch(e: any) {
+            // Response to client
+            ctx.body = e.message;
+            ctx.status = 500;
+
+            // Log results
+            logger.info(`Body: ${ctx.body}\nStatus: ${ctx.status}`);
+
+            // Clear req/res queue
+            await next();
+        }
     }
 
     /**
@@ -53,7 +171,42 @@ class CustomerController {
      * @param {() => Promise<void>} next The next client request.
      */
     public async deleteCustomers(ctx: RouterContext, next: () => Promise<void>): Promise<void> {
+        try {
+            // Find and delete customer
+            const customer: Document | null = await Customer.findByIdAndDelete(new Types.ObjectId(ctx.params.id))
 
+            // If customer was found
+            if(!_.isNil(customer)){
+                // Response to client
+                ctx.body = {message: "Success"};
+                ctx.status = 202;
+
+                // Log results
+                logger.info(`Body: ${ctx.body}\nStatus: ${ctx.status}`);
+            }
+
+            // If customer is not found
+            else {
+                // Response to client
+                ctx.body = {message: "Customer not found"};
+                ctx.status = 404;
+
+                // Log results
+                logger.info(`Body: ${ctx.body}\nStatus: ${ctx.status}`);
+            }
+            // Clear req/res queue
+            await next();
+        } catch(e: any) {
+            // Response to client
+            ctx.body = e.message;
+            ctx.status = 500;
+
+            // Log results
+            logger.info(`Body: ${ctx.body}\nStatus: ${ctx.status}`);
+
+            // Clear req/res queue
+            await next();
+        }
     }
 }
 
