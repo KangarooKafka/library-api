e8a1d90fcc6f68a6ffa38485593c27376c3e685a
Kevin Darke <99612676+KangarooKafka@users.noreply.github.com>
2023-01-27 14:40:46 -0500

    Final revisions to Customer controller and router

--- src/routes/customerRouter.ts
+++ src/routes/customerRouter.ts
@@ -1,5 +1,6 @@
 import KoaRouter from "koa-router";
 import customerController from "../controllers/customerController";
+import authorization from "../middleware/authorization";
 
 // Set up Router
 const customerRouter: KoaRouter = new KoaRouter();
@@ -12,30 +13,35 @@ const baseRoute: string = '/customer';
 // POST route for adding a customer
 customerRouter.post('add-customer',
     baseRoute,
+    authorization.validateToken,
     customerController.addCustomer
 );
 
 // GET route for getting a single customer
 customerRouter.get('get-customer',
     `${baseRoute}/:id`,
+    authorization.validateToken,
     customerController.getCustomer
 );
 
 // GET route to search customers by query or get all customers if no queries
 customerRouter.get('search-customers',
     baseRoute,
+    authorization.validateToken,
     customerController.searchCustomers
 );
 
 // PUT route for updating a customer
 customerRouter.put('update-customer',
     `${baseRoute}/:id`,
+    authorization.validateToken,
     customerController.updateCustomer
 );
 
 // DELETE route for deleting a customer
 customerRouter.delete('delete-customer',
     `${baseRoute}/:id`,
+    authorization.validateToken,
     customerController.deleteCustomer
 );
 
